05435aaeaa731949bb6e5fd1f470f07804565f08
eyerean <eptinea@gmail.com>
2018-06-08 22:22:36 +0200

    remove bootstrap dependency, add bootstrap stylesheet

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './store';
 import App from './modules/app';
 
-import 'bootstrap/dist/css/bootstrap.css';
 import 'react-datetime/css/react-datetime.css';
 import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Route path='/' component={App} />
+      <Route path="/" component={App} />
     </Router>
   </Provider>,
   document.getElementById('root')

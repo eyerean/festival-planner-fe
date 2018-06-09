e01677b700da6ec491deda456fe7756667582d27
eyerean <eptinea@gmail.com>
2018-06-09 20:26:46 +0200

    remove redux form completely

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import {reducer as fetchReducer} from 'react-redux-fetch';
-import {reducer as formReducer} from 'redux-form';
+import { reducer as fetchReducer } from 'react-redux-fetch';
 
 import appReducer from './modules/app/reducer';
 import loginReducer from './modules/login/reducer';
@@ -10,8 +9,7 @@ import festivalReducer from './modules/dashboard/reducer';
 export default combineReducers({
   app: appReducer,
   festival: festivalReducer,
-  form: formReducer,
   login: loginReducer,
   routing: routerReducer,
-  repository: fetchReducer
+  repository: fetchReducer,
 });

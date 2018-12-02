591bcdefdbc580e14a598fa4fb76237e74bd5d8c
eyerean <eptinea@gmail.com>
2018-12-02 15:04:24 +0100

    make planner data dynamic

--- src/modules/dashboard/containers/FestivalPage.js
+++ src/modules/dashboard/containers/FestivalPage.js
@@ -1,7 +1,71 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Glyphicon } from 'react-bootstrap';
+import _map from 'lodash/map';
 import { Button } from 'shared';
+
+const headData = [
+  {
+    label: 'saturday',
+    dayOrder: 0,
+    stagesCols: [
+      {
+        label: 'awesomesauce',
+        stageOrder: 0,
+      },
+      {
+        label: 'stage abc',
+        stageOrder: 1,
+      },
+    ],
+  },
+];
+
+const bodyData = [
+  {
+    timeslotOrder: 0,
+    timeslotLabel: '15:00 - 16:00',
+    artistsCols: [
+      {
+        label: 'band A',
+        amountOfTimeslots: 1,
+        stageOrder: 0,
+      },
+      {
+        label: '-',
+        amountOfTimeslots: 1,
+        stageOrder: 1,
+      },
+    ],
+  },
+  {
+    timeslotOrder: 1,
+    timeslotLabel: '16:00 - 17:00',
+    artistsCols: [
+      {
+        label: 'band ZZ',
+        amountOfTimeslots: 2,
+        stageOrder: 0,
+      },
+      {
+        label: 'famous band',
+        amountOfTimeslots: 1,
+        stageOrder: 1,
+      },
+    ],
+  },
+  {
+    timeslotOrder: 2,
+    timeslotLabel: '17:00 - 18:00',
+    artistsCols: [
+      {
+        label: 'allochiria',
+        amountOfTimeslots: 1,
+        stageOrder: 1,
+      },
+    ],
+  },
+];
 
 class FestivalPage extends React.Component {
   handleAddATimeslot = () => {
@@ -15,34 +79,39 @@ class FestivalPage extends React.Component {
           <thead>
             <tr>
               <th>hours</th>
-              <th colSpan="2">saturday</th>
+              {_map(headData, day => (
+                <th key={day.dayOrder} colSpan={day.stagesCols.length || '1'}>
+                  {day.label}
+                </th>
+              ))}
               <ButtonCell>
                 <Button primary onClick={undefined}>
                   <Glyphicon glyph="plus" />
                 </Button>
               </ButtonCell>
             </tr>
+            <SecondHeadRow>
+              <ButtonCell>
+                <Button primary onClick={this.handleAddATimeslot}>
+                  <Glyphicon glyph="plus" />
+                </Button>
+              </ButtonCell>
+              {_map(headData, day =>
+                _map(day.stagesCols, stage => <td key={stage.stageOrder}>{stage.label}</td>)
+              )}
+            </SecondHeadRow>
           </thead>
           <tbody>
-            <tr>
-              <td />
-              <td>stage 1</td>
-              <td>stage abc</td>
-            </tr>
-            <tr>
-              <td>15:00 - 16:00</td>
-              <td>band A</td>
-              <td>-</td>
-            </tr>
-            <tr>
-              <td>16:00 - 17:00</td>
-              <td rowSpan="2">band ZZ</td>
-              <td>famous band</td>
-            </tr>
-            <tr>
-              <td>17:00 - 18:00</td>
-              <td>allochiria</td>
-            </tr>
+            {_map(bodyData, row => (
+              <tr key={row.timeslotOrder}>
+                <td>{row.timeslotLabel}</td>
+                {_map(row.artistsCols, artist => (
+                  <td key={artist.stageOrder} rowSpan={artist.amountOfTimeslots || '1'}>
+                    {artist.label}
+                  </td>
+                ))}
+              </tr>
+            ))}
             <tr>
               <ButtonCell>
                 <Button primary onClick={this.handleAddATimeslot}>
@@ -83,4 +152,8 @@ const ButtonCell = styled.th`
     margin: 0;
     padding: 5px 40px;
   }
+`;
+
+const SecondHeadRow = styled.tr`
+  font-weight: bold;
 `;
